9a0bf530b7c41766f608ea950395a465c38d05e8
wang6602 <wangalan599@gmail.com>
2024-10-30 15:28:39 -0400

    Merge remote-tracking branch 'origin/main'

--- DatabaseManagerInterface.java
+++ DatabaseManagerInterface.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 
 public interface DatabaseManagerInterface {
     public ArrayList<String> getChatID();
+    public ArrayList<String> readChat();
+    public ArrayList<String> readEndChat();
+    public void newText();
+    public void deleteText();
+    public void createChat();
+    public boolean createUser();
+    public boolean removeUser();
+    public boolean removeUserFromChat();
+    public boolean addUserToChat();
+    public ArrayList<> userLookup();
+    public ArrayList<> userViewer();
+    public void updateUser();
+    public boolean loginUser();
 
 
 }
