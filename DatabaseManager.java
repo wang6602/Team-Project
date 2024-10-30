1d4d8b7ebca911fe36d6fcd31c3b5d34615995c5
wang6602 <wangalan599@gmail.com>
2024-10-30 16:05:56 -0400

    Merge remote-tracking branch 'origin/main'

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -4,8 +4,16 @@ import java.io.*;
 public class DatabaseManager implements DatabaseManagerInterface {
     public ArrayList<String> getChatIDs() {
         try {
-            FileOutputStream fos = new FileOutputStream("chatIDs.txt");
+            File f = new File("chatIDs.txt");
+            if (f.exists() == false) {
+                f.createNewFile();
+            }
+            FileReader fr = new FileReader(f);
+            BufferedReader bfr = new BufferedReader(fr);
+            String currentLine = bfr.readLine();
+            while(currentLine != null) {
 
+            }
         } catch(Exception e) {
             e.printStackTrace();
         }
