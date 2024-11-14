05c5f4800c6dfab80ec658ac909442df4ff2b94e
Rohit Sattuluri <rsattulu@purdue.edu>
2024-11-14 18:10:01 -0500

    fixed compile error

--- Client.java
+++ Client.java
@@ -9,7 +9,6 @@ public class Client {
     private String username;
     private String password;
     private String userID;
-    private String[] userID;
 
     public Client(){
         try {
@@ -129,15 +128,6 @@ public class Client {
     }
 
 
-
-
-
-    /*
-            socket = new Socket("localhost", 4242);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
-            System.out.println("Connected");
-     */
     public void deleteText(String chatID, String message){
         try {
             out.println("Deleted:" + userID + ":Chat ID:" + chatID + ":Message content:" + message);
@@ -146,9 +136,7 @@ public class Client {
         }
     }
 
-    public void createChat(String[] userID){
-        out.println("CHATCREATED:" + )
-    }
+
 
     public static void main(String[] args){
         Client client = new Client();
